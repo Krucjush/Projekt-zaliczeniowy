51b8ab31461ca18c1f751c7c6defcc94b7f62347
Dawid Studzizba <hiewiawiag@gmail.com>
2022-05-16 12:45:43 +0200

    Dodaj pliki projektów.

---
+++ Login/UsersContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public class UsersContext : DbContext
+    {
+
+    }
+}
---
+++ Test/Model.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public string ConnectionString { get; }
+
+        public BloggingContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(ConnectionString);
+        }
+    }
+
+    public class Blog
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    public class Post
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public long BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
